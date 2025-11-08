a0930ccf894db8859378e544b4170a7f6451ec75
Rabeya Akter <rabeyariya251@gmail.com>
2025-11-08 10:41:33 +0600

    analaytics

--- src/Components/Dashboard/AnalyticsInfo/AnalyticsInfo.jsx
+++ src/Components/Dashboard/AnalyticsInfo/AnalyticsInfo.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable no-unused-vars */
 import { Col } from "antd";
 import { FaUsers } from "react-icons/fa";
-import earning from "../../../assets/image/income.png";
+import { useGetAllBookingQuery } from "../../../redux/features/bookingApi/bookingApi";
+import { useState } from "react";
+// import earning from "../../../assets/image/income.png";
 
 const AnalyticsInfo = () => {
+  const [date, setDate] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const { data: allBookingData, isLoading } = useGetAllBookingQuery({
+    page: currentPage,
+    limit: pageSize,
+    date,
+  });
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div className="mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-1 gap-6">
         {/* earning */}
         <Col>
           <div className="flex flex-col justify-start  border-r-2 py-4 px-6 bg-secondary rounded-md gap-5  h-auto md:h-28">
@@ -15,13 +26,15 @@ const AnalyticsInfo = () => {
               </p>
               <div>
                 <p className="text-base md:text-2xl ">Total Booking</p>
-                <p className="text-primary text-4xl font-bold ">150.10K</p>
+                <p className="text-primary text-4xl font-bold ">
+                  {allBookingData?.data?.meta?.total}
+                </p>
               </div>
             </div>
           </div>
         </Col>
 
-        <Col>
+        {/* <Col>
           <div className="flex flex-col justify-start  border-r-2 py-4 px-6 bg-secondary rounded-md gap-5  h-auto md:h-28">
             <div className="flex  gap-3 items-cenetr">
               <p className="p-3 rounded-full bg-white flex justify-center items-center">
@@ -33,7 +46,7 @@ const AnalyticsInfo = () => {
               </div>
             </div>
           </div>
-        </Col>
+        </Col> */}
       </div>
     </div>
   );
--- src/Pages/Analytics/Analytics.jsx
+++ src/Pages/Analytics/Analytics.jsx
@@ -1,16 +1,16 @@
 import AnalyticsInfo from "../../Components/Dashboard/AnalyticsInfo/AnalyticsInfo";
-import SubscriptionGrowth from "../../Components/Dashboard/SubscriptionGrowth/SubscriptionGrowth";
-import UserGrowth from "../../Components/Dashboard/UserGrowth/UserGrowth";
+// import SubscriptionGrowth from "../../Components/Dashboard/SubscriptionGrowth/SubscriptionGrowth";
+// import UserGrowth from "../../Components/Dashboard/UserGrowth/UserGrowth";
 import RecentBooking from "../../Components/RecentBooking/RecentBooking";
 
 const Analytics = () => {
   return (
     <div>
       <AnalyticsInfo />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <SubscriptionGrowth />
         <UserGrowth />
-      </div>
+      </div> */}
       <RecentBooking />
     </div>
   );
