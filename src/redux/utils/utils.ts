99963483af3821599eae2b59725309df15295e42
Rabeya Akter <rabeyariya251@gmail.com>
2025-11-08 10:26:14 +0600

    implement admin prodile

--- src/redux/utils/utils.ts
+++ src/redux/utils/utils.ts
@@ -1,3 +1,4 @@
 // export const BASE_URL = "http://10.10.20.13:5000/api/v1";
 // export const BASE_URL = "https://smithwilliumsbackend.vercel.app/api/v1";
-export const BASE_URL = "http://13.49.243.231:5000/api/v1";
+// export const BASE_URL = "http://13.49.243.231:5000/api/v1";
+export const BASE_URL = "https://backend.desertlaila.com/api/v1";
--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
   const token = localStorage.getItem("token");
-  console.log(token);
+  console.log("token:",token);
   if (!token) return <Navigate to="/sign-in" />;
 
   return <Outlet />;
